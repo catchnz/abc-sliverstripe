3718f49e027498ee310541b563584b6a64f93369
v2 <v2@v2.local>
2019-11-04 15:36:35 +1300

    37

--- _config.php
+++ _config.php
@@ -1,5 +1,10 @@
 <?php
 
+// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+
+
 // Define path constant
 $path = str_replace('\\', '/', __DIR__);
 $path_fragments = explode('/', $path);
